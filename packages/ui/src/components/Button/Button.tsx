0ac9944e3e7c8709223ae662cf8cc241f84d4f10
Tushar Choudhari <choudharit23@gmail.com>
2025-12-06 15:11:06 +0530

    Use CSS class and clsx instead of stylex

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,23 +1,12 @@
 import { Button as RAButton, type ButtonProps } from 'react-aria-components';
-import * as stylex from '@stylexjs/stylex';
-import { spacing } from '../../tokens/spacing.stylex';
-
-const styles = stylex.create({
-  base: {
-    paddingLeft: spacing.sm,
-    paddingRight: spacing.sm,
-    paddingTop: spacing.xs,
-    paddingBottom: spacing.xs,
-  },
-});
+import clsx from 'clsx';
+import { base } from './button.css';
 
 export const Button = (props: ButtonProps) => {
-  const styleProps = stylex.props(styles.base);
   return (
     <RAButton
-      {...styleProps}
       {...props}
-      className={`${styleProps.className || ''} ${props.className || ''}`.trim()}
+      className={clsx(base, props.className)}
     />
   );
 };
