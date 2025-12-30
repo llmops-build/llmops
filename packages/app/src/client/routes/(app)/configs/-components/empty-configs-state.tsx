c8b54cb7a11d422e5e660ee70ab05b248f3a9a92
Tushar Choudhari <choudharit23@gmail.com>
2025-12-30 22:12:00 +0530

    Remove Logo from empty configs state
    Signed-off-by: Tushar Choudhari <choudharit23@gmail.com>

--- packages/app/src/client/routes/(app)/configs/-components/empty-configs-state.tsx
+++ packages/app/src/client/routes/(app)/configs/-components/empty-configs-state.tsx
@@ -1,24 +1,16 @@
-import { logoWithDarkmode } from '@client/styles/logo.css';
 import {
   emptyConfigsStateContainer,
   emptyConfigsStateContent,
   emptyConfigsStateTitle,
 } from './empty-configs-state.css';
-import Logo from '@client/components/icons/llmops.svg?react';
 
 const EmptyConfigsState = () => {
   return (
     <div className={emptyConfigsStateContainer}>
       <div className={emptyConfigsStateContent}>
-        <Logo
-          style={{ height: 128, width: 128 }}
-          className={logoWithDarkmode()}
-        />
-        <div>
-          <h2 className={emptyConfigsStateTitle}>
-            No configs yet. Create your first config to get started.
-          </h2>
-        </div>
+        <h2 className={emptyConfigsStateTitle}>
+          No configs yet. Create your first config to get started.
+        </h2>
       </div>
     </div>
   );
